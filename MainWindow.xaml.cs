0bfb6baba9b2a7c769ffa4aa66f420ca45f2976d
MrTK <dev.mrtk@gmail.com>
2020-08-10 12:54:13 +0200

    Some cleanup of old code

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -59,137 +59,33 @@ namespace ID3_Tag_Editor
 
         public void OutputCount(object sender, RoutedEventArgs e)
         {
-
-
             /*
-            string[] allFiles = FileSystem.GetFilesFromDirectory(Paths.Import, true);
+            Console.WriteLine("Title: " + Song.Tag.Title);
+            Console.WriteLine("Artist: " + Song.Tag.FirstPerformer);
+            Console.WriteLine("Album: " + Song.Tag.Album);
 
-            Console.WriteLine("The folder contains {0} songs.", allFiles.Length);
-
-            for (int i = 0; i < allFiles.Length; i++)
+            if (Modes.text != Modes.Text_Mode.none)
             {
-                if (allFiles[i].Contains(".mp3"))
+
+                if (Modes.text == Modes.Text_Mode.all || Modes.text == Modes.Text_Mode.Folders)
                 {
-                    TagLib.File Song = TagLib.File.Create(allFiles[i]);
+                    string newPath = Paths.Export + @"\" + Song.Tag.Album.Replace(":", " ").Replace("/", "_");
 
-                    if (Song.Tag.FirstPerformer != null && Song.Tag.Title != null)
+                    if (!Directory.Exists(newPath))
                     {
-                        string newFileName = Song.Tag.FirstPerformer + " - " + Song.Tag.Title.Replace(":", " ").Replace("<", "").Replace(">", "").Replace("/", "_").Replace(@"\", "_");
-                        Debug.WriteLine(newFileName);
+                        Directory.CreateDirectory(newPath);
 
-                        //Album
-                        if (string.IsNullOrEmpty(Song.Tag.Album))
-                        {
-                            if (Song.Tag.Title.ContainsAny(new string[] {"Remix", "Cover", "Flip", "Rework", "Edit", "Redo", "VIP" }))
-                            {
-                                string newName;
-
-                                //Test
-                                if (!Song.Tag.Title.Contains("["))
-                                {
-                                    if (Song.Tag.Title.Contains("("))
-                                    {
-                                        string[] Splitted = Song.Tag.Title.Split('(');
-
-                                        newName = Splitted[0].RemoveLastChar();
-                                    }
-
-                                    else
-                                    {
-                                        newName = Song.Tag.Title;
-                                    }
-                                }
-
-                                else
-                                {
-                                    string[] Splitted = Song.Tag.Title.Split('[');
-
-                                    newName = Splitted[0].RemoveLastChar();
-                                }
-
-                                Song.Tag.Album = newName + " (The Remixes)";
-                            }
-
-                            else
-                            {
-                                Song.Tag.Album = Song.Tag.Title + " - Single";
-                            }
-                        }
-
-                        else
-                        {
-                            //string[] Splitted = Song.Tag.Title.Split('[');
-                        }
-
-                        Song.Save();
-
-                        Console.WriteLine("Title: " + Song.Tag.Title);
-                        Console.WriteLine("Artist: " + Song.Tag.FirstPerformer);
-                        Console.WriteLine("Album: " + Song.Tag.Album);
-
-                        if (Modes.text != Modes.Text_Mode.none)
-                        {
-
-                            if (Modes.text == Modes.Text_Mode.all || Modes.text == Modes.Text_Mode.Folders)
-                            {
-                                string newPath = Paths.Export + @"\" + Song.Tag.Album.Replace(":", " ").Replace("/", "_");
-
-                                if (!Directory.Exists(newPath))
-                                {
-                                    Directory.CreateDirectory(newPath);
-
-                                    Debug.WriteLine("Created the directory {0}", Song.Tag.Album);
-                                }
-
-                                File.Move(allFiles[i], newPath + @"\" + newFileName + ".mp3");
-                            }
-
-                            else if (Modes.text == Modes.Text_Mode.Rename)
-                            {
-                                File.Move(allFiles[i], Paths.Export + @"\" + newFileName + ".mp3");
-                            }
-                        }
-
-                        if (Modes.image != Modes.Image_Mode.none || Modes.image_2 != Modes.Image_2_Mode.none)
-                        {
-                            var mStream = new MemoryStream();
-                            var firstPicture = Song.Tag.Pictures.FirstOrDefault();
-                            if (firstPicture != null)
-                            {
-                                byte[] pData = firstPicture.Data.Data;
-                                mStream.Write(pData, 0, Convert.ToInt32(pData.Length));
-                                var CoverImage = new Bitmap(mStream, false);
-                                mStream.Dispose();
-
-                                if (Modes.image == Modes.Image_Mode.CheckSquare || Modes.image == Modes.Image_Mode.all)
-                                {
-                                    if (CoverImage.Width != CoverImage.Height)
-                                    {
-                                        Console.WriteLine("{0} [{1} x {2}]", newFileName, CoverImage.Width, CoverImage.Height);
-                                    }
-                                }
-
-                                if (Modes.image == Modes.Image_Mode.CheckSize || Modes.image == Modes.Image_Mode.all)
-                                {
-                                    if (CoverImage.Width > 2000)
-                                    {
-                                        Console.WriteLine("{0} [{1} x {2}]", newFileName, CoverImage.Width, CoverImage.Height);
-                                    }
-                                }
-                            }
-                        }
+                        Debug.WriteLine("Created the directory {0}", Song.Tag.Album);
                     }
-                    else
-                    {
-                        Console.WriteLine("Tags are missing!");
-                    }
+
+                    File.Move(allFiles[i], newPath + @"\" + newFileName + ".mp3");
                 }
 
-                
+                else if (Modes.text == Modes.Text_Mode.Rename)
+                {
+                    File.Move(allFiles[i], Paths.Export + @"\" + newFileName + ".mp3");
+                }
             }
-
-            Preferences.Save();
-
         */
         }
 
