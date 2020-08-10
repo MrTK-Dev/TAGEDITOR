845f82e6c899aa95ac8ff2e7e55a91dc18d77865
MrTK <dev.mrtk@gmail.com>
2020-08-10 12:35:03 +0200

    New "IO.TagLibEXT.cs" for TagLib extensions
    Method to retrieve cover image from a song.

---
+++ scripts/IO/TagLibEXT.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ID3_Tag_Editor.Scripts.IO
+{
+    public static class TagLibEXT
+    {
+        public static Bitmap GetCoverImage(TagLib.File newSong)
+        {
+            MemoryStream mStream = new MemoryStream();
+
+            TagLib.IPicture firstPicture = newSong.Tag.Pictures.FirstOrDefault();
+
+            if (firstPicture != null)
+            {
+                byte[] pData = firstPicture.Data.Data;
+
+                mStream.Write(pData, 0, Convert.ToInt32(pData.Length));
+
+                Bitmap CoverImage = new Bitmap(mStream, false);
+
+                mStream.Dispose();
+
+                return CoverImage;
+            }
+
+            return null;
+        }
+    }
+}
