6c11a349dd374c1a648e75de2b35a701016c7251
MrTK <dev.mrtk@gmail.com>
2020-08-13 16:43:47 +0200

    Remove unused classes and add "Buttons.cs"

---
+++ scripts/UI/Window/Buttons.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ID3_Tag_Editor.Scripts.UI.Window
+{
+    public class Buttons
+    {
+        public void OpenEditor()
+        {
+            Debug.WriteLine("It does work - sick");
+        } 
+    }
+}
