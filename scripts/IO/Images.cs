240741f9536aa7906f1994a05af2626d6242d756
MrTK <dev.mrtk@gmail.com>
2021-03-30 10:10:16 +0200

    Removed deprecated code and documented Images.cs

--- scripts/IO/Images.cs
+++ scripts/IO/Images.cs
@@ -11,32 +11,37 @@ using System.Windows.Media.Imaging;
 
 namespace ID3_Tag_Editor.Scripts.IO
 {
+    /// <summary>
+    /// Everything related to handling images. This is about saving, caching, retrieving, etc.
+    /// </summary>
     public static class Images
     {
+        /// <summary>
+        /// Relative folder to TEMP files.
+        /// </summary>
         public static string TEMP_Folder = @"resources\TEMP";
 
+        /// <summary>
+        /// Saves a given bitmap as a ".png" file.
+        /// </summary>
+        /// <param name="bitMap">The given Bitmap that gets saved as a file.</param>
+        /// <param name="newPath">The absolute path for the new file.</param>
+        /// <param name="fileName">The name of the new file + ".png".</param>
         public static void SaveBitMapToFile(Bitmap bitMap, string newPath, string fileName)
         {
-            /*using (FileStream fileStream = new FileStream(newPath, FileMode.Create))
-            {
-                //bitMap = new Bitmap(fileStream);
-                Bitmap copy = new Bitmap(bitMap);
-                copy.Save(fileName, ImageFormat.Png);
-                bitMap.Dispose();
-                copy.Dispose();
-            }*/
-
-            Console.WriteLine(Path.Combine(newPath, fileName));
-
             Bitmap copy = new Bitmap(bitMap);
             copy.Save(newPath + @"\" + fileName + ".png", ImageFormat.Png);
+
             bitMap.Dispose();
             copy.Dispose();
-
-            //bitMap.Save(newPath);
-            //bitMap.Dispose();
         }
 
+        /// <summary>
+        /// Caches a given Bitmap to a temporary file and returns the absolute path.
+        /// </summary>
+        /// <param name="bitMap">The given Bitmap that gets cached.</param>
+        /// <param name="fileName">The name of the new file + ".png".</param>
+        /// <returns>The absolute path to the new file.</returns>
         public static string CacheBitMap(Bitmap bitMap, string fileName)
         {
             string newPath = Paths.GetFullPath(TEMP_Folder);
@@ -46,11 +51,19 @@ namespace ID3_Tag_Editor.Scripts.IO
             return Path.Combine(newPath, fileName + ".png");
         }
 
+        /// <summary>
+        /// This clears the temporary cache folder. (<see cref="TEMP_Folder"/>)
+        /// </summary>
         public static void ClearCache()
         {
             FileSystem.ClearFolder(TEMP_Folder, true);
         }
 
+        /// <summary>
+        /// This caches the cover of a song file and returns the BitmapImage. If the cover is null a placeholder image gets returned.
+        /// </summary>
+        /// <param name="newFile">The given TagLib.File.</param>
+        /// <returns>The cover or a placeholder as a BitmapImage.</returns>
         public static BitmapImage GetCoverUI(TagLib.File newFile)
         {
             Bitmap bitmap = TagLibEXT.GetCoverImage(newFile);
